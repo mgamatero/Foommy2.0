70f9b69b7baf9e40817e00f54d8406c4ea87c248
Mike Gamatero <mgamatero@yahoo.com>
2020-05-05 09:39:01 -0700

    updated chef object for non logged in users

--- src/app/services/non-logged-on-data.service.ts
+++ src/app/services/non-logged-on-data.service.ts
@@ -9,29 +9,54 @@ export class NonLoggedOnDataService {
     this.nonLoggedOnChefs = [
       {
         id: 'AAAAA',
+        aboutKitchen:
+          'Welcome to my humble kitchen.  I will cook in front of you, enjoy the food, company and make a friend in me.',
         chefName: 'Mike Non Logged On',
-        image: 'https://image.flaticon.com/icons/png/512/306/306003.png',
-        cuisine: 'Mediterranean',
-        diningOptions: ['Dine in','Takeout'],
-        location:"Tustin,CA",
-hours: 'M-F 11:30-9PM',
-theDiningArea:'We have a 6 seat table in our family patio. Eat with our family, or with privacy as you need.',
-howToGetThere:'We are really close to the Tustin Ranch exit by the 5 fwy',
-additionalInfo:'Our dining experience is best for quiet and relaxed times with family/friends.  Loud behavior is not tolerated.'
+        placeDesc:
+          'Homely little dining area.  Pretty casual vibe here.  Staff and family members are part of this kitchen as we serve you.',
+          theDiningArea:
+           'We have a 6 seat table in our family patio. Eat with our family, or with privacy as you need.',
+         additionalInfo:
+           'Alcohol is not served.  You can bring a bottle of wine, only if you share',
+        street: '1234 Any St',
+        city: 'Tustin',
+        state: 'CA',
+        howToGetThere:
+          'We are by the 5 fwy, Tustin Ranch exit.  Close to Costco',
+        diningOptions: ['Dine in', 'Takeout'],
+        paymentMethod: ['Venmo', 'ApplePay', 'Paypal', 'Cash', 'Other'],
+        availability: 'MWF noontime only',
+        seatingCapacity: '6 guests',
+        advanceReservation: 'Same Day',
+        prepTime: '2 day notice',
+        image: 'https://assets.kraftfoods.com/recipe_images/opendeploy/118519_640x428.jpg'
+
       },
       {
         id: 'BBBBB',
-        chefName: '2nd Mike Non Logged On',
+        aboutKitchen:
+          'Welcome to my humble kitchen.  I will cook in front of you, enjoy the food, company and make a friend in me.',
+        chefName: '2nd Chef - Non Logged On',
+        placeDesc:
+          'Family kitchen to experience for you. This is loud, fun, casual.',
+          theDiningArea:
+          'Nice patio setup under a string of patio lights.',
+        additionalInfo:
+          'Kitchen staff and family members are in/out of my kitchen.',
+          street: '9987 Main St',
+        city: 'Anaheim Hills',
+        state: 'CA',
+        howToGetThere:
+          '55/91 Fwy intersection, close to Shell gas station.',
+        diningOptions: ['Dine in'],
+        paymentMethod: ['Venmo', 'ApplePay', 'Paypal', 'Cash', 'Other'],
+        availability: 'MWF noontime only',
+        seatingCapacity: '4 guests',
+        advanceReservation: 'Same Day',
+        prepTime: '2 day notice',
         image:
-          'https://www.1400foodlab.com/wp-content/uploads/Chef-icon-600x600.jpg',
-        cuisine: 'Indian',
-        diningOptions: ['Dine in','Takeout'],
-        location:"Costa Mesa,CA",
-        hours: 'Sat-Sun Noontime only',
-        theDiningArea:'Table of 8 at our garage area.',
-        howToGetThere:'Close to the 405f fwy, Harbor exit',
-        additionalInfo:'Bring alcohol but you need to share with the host.  No rude behavior.  Kids welcome'
-      }
+          'https://tse1.mm.bing.net/th?id=OIP.8D_rb6Akp-e8rX0_bOYYkwHaFj&pid=Api&P=0&w=231&h=174'
+             },
     ];
   }
 
@@ -40,10 +65,10 @@ additionalInfo:'Our dining experience is best for quiet and relaxed times with f
   }
 
   getNonLoggedOnDataByID(id): any {
-    const byId = this.nonLoggedOnChefs.filter(chefs=>{
-      return chefs.id === id
+    const byId = this.nonLoggedOnChefs.filter((chefs) => {
+      return chefs.id === id;
     });
     // console.log('tempchefbyid: ', byId)
-    return byId
+    return byId;
   }
 }
