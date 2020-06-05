93d41f591dbc3d7fcfd66e40c06dbed3e809f5c2
Mike Gamatero <mgamatero@yahoo.com>
2020-06-05 14:39:51 -0700

    need to fix getDishbyID

--- src/app/page-not-logged-dish-details/page-not-logged-dish-details.component.ts
+++ src/app/page-not-logged-dish-details/page-not-logged-dish-details.component.ts
@@ -10,6 +10,8 @@ import { NonLoggedOnDataService } from '../services/non-logged-on-data.service';
 export class PageNotLoggedDishDetailsComponent implements OnInit {
 tempChefId:string;
 tempDishId:string;
+tempChef:any;
+tempDish:any;
 
   constructor(
     private route: ActivatedRoute,
@@ -17,12 +19,20 @@ tempDishId:string;
     private tempData: NonLoggedOnDataService
   ) {
 
-//Chef data here
+//Chef and Dish IDs here
 this.route.paramMap.subscribe((params: ParamMap) => {
   this.tempChefId = params.get('chefId');
   this.tempDishId = params.get('dishId');
 });
 
+this.tempChef = this.tempData.getNonLoggedOnChefByID(
+  this.tempChefId
+)[0];
+
+
+//Dish data here - CHEAT for now.  Hardcoded in Service
+this.tempDish = this.tempData.getNonLoggedOnDishesByID(this.tempChefId);
+console.log(this.tempDish)
   }
 
   ngOnInit(): void {
