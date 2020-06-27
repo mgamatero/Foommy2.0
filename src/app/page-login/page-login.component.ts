189849bf80f8cd51b1bd1ac40c357bd999905d8a
Mike Gamatero <mgamatero@yahoo.com>
2020-06-27 09:41:53 -0700

    login form

--- src/app/page-login/page-login.component.ts
+++ src/app/page-login/page-login.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-page-login',
   templateUrl: './page-login.component.html',
-  styleUrls: ['./page-login.component.css']
+  styleUrls: ['./page-login.component.css'],
 })
 export class PageLoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private auth: AuthService
+  ) {
+    // redirect to home if already logged in
+    // if (this.auth.isLogged) {
+    //   this.router.navigate(['/']);
+    // }
   }
 
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.auth
+      .emailLogin(this.f.username.value, this.f.password.value)
+      .then((data) => {
+        console.log('Logged in');
+      });
+  }
 }
