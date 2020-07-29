d51ab4fd76d6a43d3d71b1e54be059db39b45a28
Mike Gamatero <mgamatero@yahoo.com>
2020-07-29 11:33:12 -0700

    semi-updated userObj service.  starting to create chefForm

--- src/app/page-become-a-chef-form/page-become-a-chef-form.component.ts
+++ src/app/page-become-a-chef-form/page-become-a-chef-form.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+  FormArray,
+} from '@angular/forms';
+import { UserObjService } from '../services/user-obj.service';
 
 @Component({
   selector: 'app-page-become-a-chef-form',
   templateUrl: './page-become-a-chef-form.component.html',
-  styleUrls: ['./page-become-a-chef-form.component.css']
+  styleUrls: ['./page-become-a-chef-form.component.css'],
 })
 export class PageBecomeAChefFormComponent implements OnInit {
+chefSignUpForm : FormGroup;
 
-  constructor() { }
+  constructor(
+    private userObj: UserObjService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.chefSignUpForm = this.formBuilder.group({
+      foommyImage: [''],
+      chefName: [''],
+      placeDesc: [''],
+      theDiningArea: [''],
+      additionalInfo: [''],
+      availableDays: [''],
+      availableTimes: [''],
+      contactEmail: [''],
+      contactPhone: [''],
+      city: [''],
+      state: [''],
+      howToGetThere: [''],
+           diningOptions:this.formBuilder.array([this.formBuilder.control('')]),
+      paymentMethod:this.formBuilder.array([this.formBuilder.control('')]),
+      availability: [''],
+      seatingCapacity: [''],
+      advanceReservation: [''],
+      prepTime: [''],
+      cuisineType: [''],
+    });
   }
-
 }
--- src/app/services/user-obj.service.ts
+++ src/app/services/user-obj.service.ts
@@ -7,6 +7,7 @@ import {
 import { Observable, Subscriber } from 'rxjs';
 import { of } from 'rxjs';
 import { AuthService } from './auth.service';
+import{ ChefModel } from  '../models/chefModel';
 
 @Injectable({
   providedIn: 'root',
@@ -33,5 +34,56 @@ export class UserObjService {
   // ----------------------GET USER OBJECT BY ID---------------------
   getUserObjectByID(id) {
     return this.db.doc(`users/${id}`).snapshotChanges();
+  }
+
+  // ---------------------ADD FOOMMY DETAILS ---------------------------
+  /*  --- use this!!!!
+   this.ulamForm = this.formBuilder.group({
+      ulamName: [''],
+      ulamImage: [''],
+      ulamPrice: [''],
+      ulamSize: [''],
+      soldOut:[false]
+    });
+     }
+
+  clearForms(){
+    this.ulamForm.reset();
+  }
+
+  addFromForm(){
+    this.dbService.addUlam(this.ulamForm.value);
+    this.ulamForm.reset();
+  }*/
+  addChefDetails(id, isChef=true) {
+    this.db
+      .collection('users')
+      .doc(id)
+      .set({
+        isChef
+  //       foommyImage: string;
+  // chefName: string;
+  // aboutKitchen: string;
+  // placeDesc: string;
+  // theDiningArea: string;
+  // additionalInfo: string;
+  // availableDays: string;
+  // availableTimes: string;
+  // contactEmail: string;
+  // contactPhone: string;
+  // city: string;
+  // state: string;
+  // howToGetThere: string;
+  // diningOptions: string[];
+  // paymentMethod: string[];
+  // availability: string;
+  // seatingCapacity: string;
+  // advanceReservation: string;
+  // prepTime: string;
+  // cuisineType: string;
+      })
+      .then(() => {
+        window.alert(`User ${id} added to user document`);
+      });
   }
 }
